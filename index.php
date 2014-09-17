31b4813b9bcdf52cd3001aff7a6659cf3c36dc8b
schwag <justin@myriadcore.com>
2014-09-17 09:47:15 -0400

    Integrated PHP

---
+++ index.php
@@ -0,0 +1,184 @@
+<?php include "php/header.php";?>
+
+<body>
+                <!-- Top White Address Container -->
+                <section class="whiteBackground navbar-fixed-top">
+                    <div class="container topSocial">
+                        <span class="glyphicon glyphicon-search"></span>
+                    </div>
+                </section>
+
+                <!-- Top White Logo Container -->
+                <section class="logoBackground navbar-fixed-top">
+                    <div class="container logoSection">
+                        <header class="row">
+                            <div class="col-lg-6 col-sm-5">
+                                <a href="index.html"><img src="img/logo.png" alt="RDL Supply" class="img-responsive"></a>
+                            </div>
+                            <div class="col-lg-6 col-sm-7 headercontactInfo">
+                                <ul class="contactNumbers">
+                                    <li class="list-group-item"><span class="glyphicon glyphicon-envelope"></span> 
+                                    <a href="mailto:youremailaddress">SALES@RDLSUPPLY.COM</a></li>
+                                  <li class="list-group-item"><span class="glyphicon glyphicon-phone-alt"></span> <a href="tel:214-630-3965">214.630.3965</a></li>
+                                </ul>
+                            </div>
+                        </header>
+                    </div>
+                </section>
+
+
+            <!-- Header Navigation -->
+
+        <section class="officialNavigation">
+            <div class="container">  
+                <div class="row">
+                    <nav class="navbar navbar-default navbar-fixed-top navbar-inverse">
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#collapse">
+                              <span class="sr-only">Toggle navigation</span>
+                              <span class="glyphicon glyphicon-arrow-down"></span>
+                              MENU
+                            </button>
+                        </div>
+                        <div class="collapse navbar-collapse" id="collapse">
+                            <ul class="nav navbar-nav">
+                                <li class="active"><a href="#">Home</a></li>
+                                <li><a href="#">About</a></li>
+                                <li><a href="#">Products</a></li>
+                                <li><a href="#">Customer Service</a></li>
+                                <li><a href="#">Our Work</a></li>  
+                                <li><a href="#">Contact</a></li> 
+                            </ul> 
+                        </div>
+                     </nav> 
+                </div>
+            </div>
+        </section>
+
+            <!-- Jumbotron -->
+            <!--<div class="container">          
+                 <div class="jumbotron">
+                    <img src="">
+                    <h4>Hey</h4>
+                    <p>This is where the slider will go!</p>
+                 </div>
+            </div>-->
+
+<section class="jumboSlide">
+   <!-- <div class="container">-->
+        <div class="jumbotron">
+
+            <div id="rdl-slider" class="carousel slide" data-ride="carousel" data-interval="5500">
+              <!-- Indicators -->
+              <ol class="carousel-indicators">
+                <li data-target="#rdl-slider" data-slide-to="0" class="active"></li>
+                <li data-target="#rdl-slider" data-slide-to="1"></li>
+                <li data-target="#rdl-slider" data-slide-to="2"></li>
+              </ol>
+             
+              <!-- Wrapper for slides -->
+              <div class="carousel-inner">
+                <div class="item active">
+                  <img src="img/slide.png" alt="...">
+                  <div class="carousel-caption">
+                      <h3>Caption Text</h3>
+                  </div>
+                </div>
+                <div class="item">
+                  <img src="img/product_example_1.png" alt="...">
+                  <div class="carousel-caption">
+                      <h3>Caption Text</h3>
+                  </div>
+                </div>
+                <div class="item">
+                  <img src="img/product_example_1.png" alt="...">
+                  <div class="carousel-caption">
+                      <h3>Caption Text</h3>
+                  </div>
+                </div>
+              </div>
+             
+              <!-- Controls -->
+              <a class="left carousel-control" href="#rdl-slider" role="button" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left"></span>
+              </a>
+              <a class="right carousel-control" href="#rdl-slider" role="button" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right"></span>
+              </a>
+            </div> <!-- Carousel -->
+        </div>
+   <!-- </div>-->
+</section>
+
+
+
+
+            <section class="grayBackground">
+                <div class="container">
+
+
+                     <div class="row">
+                        <div class="sherrieSection">
+                            <div class="col-md-7">
+                                <div class="sherrieBox">
+                                    <h2>Sherrie’s Door is Always Open</h2>
+                                    <blockquote><b>"</b>As the owner of RDL Supply I invite you to contact me directly with any concerns, questions, or feedback you have. My door is always open.<b>"</b> <img src="img/sherriemartin.png" alt"" class="img-responsive"></blockquote>
+
+                                    <p><a href="#" class="btn btn-info">Click here to contact Sherrie</a></p>
+                                    <p>RDL Supply stocks more doors, accessories, and Rediframe products than any other supplier. We are a family-run, woman-owned company and we’ve been opening doors since 1981.</p>
+                                </div>
+                            </div>
+                            <div class="col-md-5">
+                                <div class="homeBuilding">    
+                                    <img src="img/rdl_building.png" alt"" class="img-responsive">  
+                                </div>           
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+
+    <section class="certificationRow">
+        <div class="container">
+                    <div class="row">
+                        <div class="certificationBox">
+                            <div class="col-md-2 col-md-offset-1 col-xs-6">
+                                <div class="certifications">
+                                    <img src="img/cfdi.png" alt"" class="img-responsive"> 
+                                </div>
+                            </div>
+                        </div>
+                        <div class="certificationBox">
+                            <div class="col-md-2 col-xs-6">
+                                <div class="certifications">
+                                    <img src="img/dhi.png" alt"" class="img-responsive"> 
+                                </div>
+                            </div>
+                        </div>
+                        <div class="certificationBox">
+                            <div class="col-md-2 col-xs-6">
+                                <div class="certifications">
+                                    <img src="img/cr.png" alt"" class="img-responsive"> 
+                                </div>
+                            </div>
+                        </div>
+                        <div class="certificationBox">
+                            <div class="col-md-2 col-xs-6">
+                                <div class="certifications">
+                                    <img src="img/wbe.png" alt"" class="img-responsive"> 
+                                </div>
+                            </div>
+                        </div>
+                        <div class="certificationBox">
+                            <div class="col-md-2 col-xs-6">
+                                <div class="certifications">
+                                    <img src="img/leedap.png" alt"" class="img-responsive"> 
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+        </div> <!-- end container -->
+    </section>
+
+<?php include "php/footer.php";?>
---
+++ php/header.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>RDL Supply</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+    <script src="js/respond.js"></script>
+</head>
