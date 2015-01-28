aa86f1dd0fd85641bbc964709ee48fca253a5fc7
schwag <justin@myriadcore.com>
2015-01-28 10:08:22 -0500

    Modal Changes

--- our-work.php
+++ our-work.php
@@ -11,34 +11,41 @@
                         <div class="col-lg-12">
 
                     <div class="row">
-                        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-                                <div class="galleryimageBackground">
-                                    <div class="galleryHover">
-                                        <a href="img/gallery_placeholder.jpg" class="img-responsive" data-lightbox="roadtrip">
-                                        <h4>View Larger</h4>
-                                        <img src="img/gallery_image_thumb_example.png" alt"" class="img-responsive"></a>
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            
+
+
+
+                                    <div class="learnMore">
+                                        <img src="img/gallery_placeholder.jpg" class="img-responsive">
+                                        <!-- Large modal -->
+                                        <button type="button" class="btn btn-info" data-toggle="modal" data-target=".modalTwo">View More</button>
+                                        <div class="modal fade modalTwo" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                                          <div class="modal-dialog modal-lg">
+                                            <div class="modal-content">
+                                              <div class="modal-header">
+                                                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                                                <h1 class="needsbodyHeading">ASSEMBLY</h1>
+                                              </div>
+                                              <div class="modal-body">
+                                                    <div>
+                                            <iframe class="adjustableVideo" width="100%" height="500" src="//www.youtube.com/embed/WT8P81oZWOI?list=UUauWBtjoD8Sbkm27cs1b1BA" frameborder="0" allowfullscreen></iframe>
+                                                    </div> 
+                                              </div>
+                                              <div class="modal-footer">
+                                                <button type="button" class="btn btn-info" data-dismiss="modal">Close</button>
+                                              </div>
+                                            </div><!-- /.modal-content -->
+                                          </div>
+                                        </div>
                                     </div>
-                                </div>
+
+
+
+                                    
+                               
                         </div>
-                        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-                                <div class="galleryimageBackground">
-                                    <div class="galleryHover">
-                                        <a href="img/gallery_placeholder.jpg" class="img-responsive" data-lightbox="roadtrip">
-                                        <h4>View Larger</h4>
-                                        <img src="img/gallery_image_thumb_example.png" alt"" class="img-responsive"></a>
-                                    </div>
-                                </div>
-                        </div>
-                        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-                                <div class="galleryimageBackground">
-                                    <div class="galleryHover">
-                                        <a href="img/gallery_placeholder.jpg" class="img-responsive" data-lightbox="roadtrip">
-                                        <h4>View Larger</h4>
-                                        <img src="img/gallery_image_thumb_example.png" alt"" class="img-responsive"></a>
-                                    </div>
-                                </div>
-                        </div>
-                        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
                                 <div class="galleryimageBackground">
                                     <div class="galleryHover">
                                         <a href="img/gallery_placeholder.jpg" class="img-responsive" data-lightbox="roadtrip">
@@ -87,46 +94,6 @@
                                 </div>
                         </div>
                     </div><!-- end of product row -->
-
-                    <div class="row">
-                        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-                                <div class="galleryimageBackground">
-                                    <div class="galleryHover">
-                                        <a href="img/gallery_placeholder.jpg" class="img-responsive" data-lightbox="roadtrip">
-                                        <h4>View Larger</h4>
-                                        <img src="img/gallery_image_thumb_example.png" alt"" class="img-responsive"></a>
-                                    </div>
-                                </div>
-                        </div>
-                        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-                                <div class="galleryimageBackground">
-                                    <div class="galleryHover">
-                                        <a href="img/gallery_placeholder.jpg" class="img-responsive" data-lightbox="roadtrip">
-                                        <h4>View Larger</h4>
-                                        <img src="img/gallery_image_thumb_example.png" alt"" class="img-responsive"></a>
-                                    </div>
-                                </div>
-                        </div>
-                        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-                                <div class="galleryimageBackground">
-                                    <div class="galleryHover">
-                                        <a href="img/gallery_placeholder.jpg" class="img-responsive" data-lightbox="roadtrip">
-                                        <h4>View Larger</h4>
-                                        <img src="img/gallery_image_thumb_example.png" alt"" class="img-responsive"></a>
-                                    </div>
-                                </div>
-                        </div>
-                        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-                                <div class="galleryimageBackground">
-                                    <div class="galleryHover">
-                                        <a href="img/gallery_placeholder.jpg" class="img-responsive" data-lightbox="roadtrip">
-                                        <h4>View Larger</h4>
-                                        <img src="img/gallery_image_thumb_example.png" alt"" class="img-responsive"></a>
-                                    </div>
-                                </div>
-                        </div>
-                    </div><!-- end of product row -->
-
 
                         </div>
                     </div>
